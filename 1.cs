90ad5fdd1aa2b852fa52af0404ae098f4a5fbee5
jadmsd <jadmassaad.astro@gmail.com>
2024-09-15 18:25:47 +0300

    init

---
+++ 1.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace My_Awesome_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("i walk into a bar and say ...");
+            Console.ReadLine();
+            Console.WriteLine("hello sir,what do you want to drink?");
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.red;
+            Console.WriteLine("a dragon came and ate everybody");
+            Console.ReadLine()
+        
+        }
+    }
+}
